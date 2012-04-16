791828e6811c640b97101d61649bdf558ea36ec0
dmadruga <dmadruga@338509.no-reply.drupal.org>
2012-04-15 19:09:49 -0600

    Issue #1056214 by dmadruga: Fixed IE7 Error: Expected identifier, string or number.

--- autologout.js
+++ autologout.js
@@ -3,26 +3,26 @@
 Drupal.behaviors.autologout = function (context) {
   var t = setTimeout(init, Drupal.settings.autologout.timeout);
   var paddingTimer;
-  
+
   function init() {
     if (Drupal.settings.autologout.jquery_ui) {
       dialog();
       paddingTimer = setTimeout(logout, Drupal.settings.autologout.timeout_padding);
     } else {
       if (confirm(Drupal.settings.autologout.message) ) {
-        refresh();    
+        refresh();
       } else {
         logout();
       }
-    }    
+    }
   }
-  
+
   function dialog() {
     $('<div> ' +  Drupal.settings.autologout.message + '</div>').dialog({
       modal: true,
       closeOnEscape: false,
       width: 'auto',
-      buttons: { 
+      buttons: {
         Reset: function() {
           $(this).dialog("destroy");
           clearTimeout(paddingTimer);
@@ -48,11 +48,11 @@ Drupal.behaviors.autologout = function (context) {
         t = setTimeout(init, Drupal.settings.autologout.timeout);
       },
       error: function(XMLHttpRequest, textStatus) {
-        alert('There has been an error resetting your last access time: ' + textStatus + '.')
-      },
+        alert('There has been an error resetting your last access time: ' + textStatus + '.');
+      }
     });
   }
-  
+
   function logout() {
     $.ajax({
       url: Drupal.settings.basePath + 'autologout_ahah_logout',
@@ -61,8 +61,8 @@ Drupal.behaviors.autologout = function (context) {
         document.location.href = Drupal.settings.autologout.redirect_url;
       },
       error: function(XMLHttpRequest, textStatus) {
-        alert('There has been an error attempting to logout: ' + textStatus + '.')
-      },
+        alert('There has been an error attempting to logout: ' + textStatus + '.');
+      }
     });
   }
 };
