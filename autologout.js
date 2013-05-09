c2d9a0f9fe7792997b59eea0fe18d5653d4a055f
John Ennew <johne@deeson.co.uk>
2013-05-09 21:03:20 +0100

    Issue #1979550 by ceng: Backported translation of yes and no buttons on version 6.x.4.x

--- autologout.js
+++ autologout.js
@@ -1,11 +1,11 @@
 // $Id:
 
 Drupal.behaviors.autologout = function (context) {
-  if(context == document) {
+  if (context == document) {
     var t = setTimeout(init, Drupal.settings.autologout.timeout);
     var paddingTimer;
   }
-  
+
   function init() {
     if (Drupal.settings.autologout.jquery_ui) {
       paddingTimer = setTimeout(confirmLogout, Drupal.settings.autologout.timeout_padding);
@@ -17,40 +17,44 @@ Drupal.behaviors.autologout = function (context) {
         url : Drupal.settings.basePath + 'autologout_ajax_get_time_left',
         dataType: 'json',
         success: function(data) {
-          window.console.log(data.time);
-          if(data.time > 0) {
+          if (data.time > 0) {
             clearTimeout(paddingTimer);
             t = setTimeout(init, data.time);
-          } else {
+          }
+          else {
             theDialog = dialog();
           }
         }
       });
-    } else {
+    }
+    else {
       if (confirm(Drupal.settings.autologout.message) ) {
-        refresh();    
-      } else {
+        refresh();
+      }
+      else {
         logout();
       }
-    }    
+    }
   }
-  
+
   function dialog() {
-    return $('<div> ' +  Drupal.settings.autologout.message + '</div>').dialog({
+    var buttons = {};
+    buttons[Drupal.t('Yes')] = function() {
+      $(this).dialog("destroy");
+      clearTimeout(paddingTimer);
+      refresh();
+    };
+
+    buttons[Drupal.t('No')] = function() {
+      $(this).dialog("destroy");
+      logout();
+    };
+
+    return $('<div>' +  Drupal.settings.autologout.message + '</div>').dialog({
       modal: true,
       closeOnEscape: false,
       width: 'auto',
-      buttons: { 
-        Reset: function() {
-          $(this).dialog("destroy");
-          clearTimeout(paddingTimer);
-          refresh();
-        },
-        Logout: function() {
-          $(this).dialog("destroy");
-          logout();
-        }
-      },
+      buttons: buttons,
       title: Drupal.settings.autologout.title,
       close: function(event, ui) {
         logout();
@@ -66,11 +70,11 @@ Drupal.behaviors.autologout = function (context) {
         t = setTimeout(init, Drupal.settings.autologout.timeout);
       },
       error: function(XMLHttpRequest, textStatus) {
-        alert('There has been an error resetting your last access time: ' + textStatus + '.')
-      },
+        alert('There has been an error resetting your last access time: ' + textStatus + '.');
+      }
     });
   }
-  
+
   // A user could have used the reset button on the tab/window they're actively
   // using, so we need to double check before actually logging out.
   function confirmLogout() {
@@ -81,7 +85,7 @@ Drupal.behaviors.autologout = function (context) {
       error: logout,
       success: function(data) {
         window.console.log(data.time);
-        if(data.time > 0) {
+        if (data.time > 0) {
           t = setTimeout(init, data.time);
         }
         else {
@@ -99,8 +103,8 @@ Drupal.behaviors.autologout = function (context) {
         document.location.href = Drupal.settings.autologout.redirect_url;
       },
       error: function(XMLHttpRequest, textStatus) {
-        alert('There has been an error attempting to logout: ' + textStatus + '.')
-      },
+        alert('There has been an error attempting to logout: ' + textStatus + '.');
+      }
     });
   }
 };
