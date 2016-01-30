67611bf7f2911692b8a45d4a5006fdcc7275cedc
Rohit Tiwari <rohit.tiwari@prohaktiv.com>
2016-01-30 13:40:06 +0530

    Added some fixes

--- src/Tests/AutologoutTest.php
+++ src/Tests/AutologoutTest.php
@@ -298,7 +298,6 @@ class AutologoutTest extends WebTestBase {
       ->save();
 
     // Check that the user can access the page after login.
-    $var = $this->drupalGet('admin/reports/status');
     $this->assertResponse(200, 'Admin page is accessible');
     $this->assertText(t("Here you can find a short overview of your site's parameters as well as any problems detected with your installation."), 'User can access elements of the admin page.');
 
